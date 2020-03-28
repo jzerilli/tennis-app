a7e4fcebded764cf16f1ae04ac543076711707ce
jzerilli <jzeril01@tufts.edu>
2020-03-28 15:52:04 -0400

    Updates

---
+++ precache-manifest.fcc52590fc060e72f22f90266c0a94ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f976c96fc181d2911a0c083b2c78310b",
+    "url": "/tennis-app/index.html"
+  },
+  {
+    "revision": "1a5e4c0e453e6b123914",
+    "url": "/tennis-app/static/css/main.d55a731c.chunk.css"
+  },
+  {
+    "revision": "fc40e0a13054fa9e57c8",
+    "url": "/tennis-app/static/js/2.029dae24.chunk.js"
+  },
+  {
+    "revision": "c3f4a91b08326551c3d2c23a3296dfa8",
+    "url": "/tennis-app/static/js/2.029dae24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5e4c0e453e6b123914",
+    "url": "/tennis-app/static/js/main.118007a5.chunk.js"
+  },
+  {
+    "revision": "2918a31c981cdb3c20e3",
+    "url": "/tennis-app/static/js/runtime-main.20e5100f.js"
+  },
+  {
+    "revision": "dfb0f719e4564488e18a172707646c9f",
+    "url": "/tennis-app/static/media/court.dfb0f719.jpg"
+  }
+]);
